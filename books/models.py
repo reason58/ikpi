f87bdc8a97a0f3b90e125a0722eb9ba9e443fb0f
Will Geiger <williamjgeiger@gmail.com>
2018-01-11 13:06:49 -0800

    Refactored books database to include pages, words, etc.

--- books/models.py
+++ books/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+class Book(models.Model):
+    book_title = models.CharField(max_length=200)
+    book_length_pages = models.IntegerField(default=0, blank=True)
+    book_length_words = models.IntegerField(default=0, blank=True)
+    book_pub_date = models.DateTimeField(default=timezone.now())
+    def __str__(self):
+        return self.book_title
+
+class Page(models.Model):
+    pub_date = models.DateTimeField('date published')
+    user_page_input = models.IntegerField()
+    def __str__(self):
+        return self.user_page_input
